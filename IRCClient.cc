404f78fd9316ffed0a505e0cbf95801a914bba63
Tarang B Khanna <khanna17@data.cs.purdue.edu>
2015-04-20 19:50:41 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -130,15 +130,17 @@ int main( int   argc,
     gtk_widget_show (messages);
     // Add messages text. Use columns 0 to 4 (exclusive) and rows 4 to 7 (exclusive) 
 
-    gdk_color_parse ("red", &color);
-
-    gtk_widget_modify_fg (widget, GTK_STATE_NORMAL,    &color);
+    
     myMessage = create_text ("I am fine, thanks and you?\n");
     gtk_table_attach_defaults (GTK_TABLE (table), myMessage, 0, 4, 5, 7);
     gtk_widget_show (myMessage);
 
     // Add send button. Use columns 0 to 1 (exclusive) and rows 4 to 7 (exclusive)
     GtkWidget *send_button = gtk_button_new_with_label ("Send");
+
+    gdk_color_parse ("red", &color);
+
+    gtk_widget_modify_fg (send_button, GTK_STATE_NORMAL,    &color);
     gtk_table_attach_defaults(GTK_TABLE (table), send_button, 0, 1, 7, 8); 
     gtk_widget_show (send_button);
     
