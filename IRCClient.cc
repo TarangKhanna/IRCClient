1f02a62ec9f2b1b28943a290eeeadbea07bbd916
Tarang B Khanna <khanna17@data.cs.purdue.edu>
2015-04-21 01:00:18 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -148,12 +148,9 @@ int main( int   argc,
     gtk_widget_show (myMessage);
 
     // Add send button. Use columns 0 to 1 (exclusive) and rows 4 to 7 (exclusive)
-    GtkWidget *send_button = gtk_button_new_with_label ("Send");
+    GtkWidget *send_button = gtk_button_new_with_label ("Create");
     //gtk_table_attach_defaults(GTK_TABLE (table), send_button, 0, 1, 7, 8); 
     gtk_widget_show (send_button);
-     GtkWidget *create_account = gtk_button_new_with_label ("Create Account");
-    gtk_table_attach_defaults(GTK_TABLE (table), send_button, 0, 1, 7, 8); 
-    gtk_widget_show (create_account);
     gtk_widget_show (table);
     gtk_widget_show (window);
     
