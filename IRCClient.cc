c06ec13fd18405468bb79f11d0d4cf6deae70369
Tarang B Khanna <khanna17@data.cs.purdue.edu>
2015-04-27 20:33:42 -0400

    Make IRCclient

--- IRCClient.cc
+++ IRCClient.cc
@@ -258,7 +258,7 @@ void send_message() {
   char* room_1 = strdup(args);
   if(strcmp(((char *) gtk_entry_get_text(GTK_ENTRY(messageEntry))),"") == 0) {
     //printf("Empty Message\n");
-    gtk_label_set_text(GTK_LABEL(currentStatus), "Can't Empty Message");
+    gtk_label_set_text(GTK_LABEL(currentStatus), "Message Empty");
   } else {
   strcat(room_1, (char *) gtk_entry_get_text(GTK_ENTRY(messageEntry)));
   sendCommand(host, port, "SEND-MESSAGE", user, password, room_1, response);
